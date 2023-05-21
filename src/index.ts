81b4aac09d7c504e293bdd612b447642590f893f
NoamSalem <35778296+NoamSalem@users.noreply.github.com>
2023-05-21 21:10:17 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { config } from 'dotenv';
+import { Client, GatewayIntentBits, ChannelType, PermissionsBitField } from 'discord.js';
+
+const token = process.env.CLIENT_TOKEN;
+config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberAdd', async member => {
+    const guildId = member.guild.id;
+    const guild = member.guild;
+    const memberCount = guild.memberCount;
+    const channels = guild.channels;
+    const name = `『👪』Members ${memberCount}`;
+
+    await channels.create({
+        type: ChannelType.GuildVoice,
+        position: 0,
+        permissionOverwrites: [
+            {
+                id: guild.roles.everyone,
+                deny: [PermissionsBitField.Flags.ReadMessageHistory]
+            },
+        ],
+        name
+    });
+});
+
+//make sure this line is the last line
+client.login(token); //login bot using token
